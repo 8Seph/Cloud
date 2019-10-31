d78005314112259523bf0f9657ae24e76902a62e
8Seph <evem-w@outlook.com>
2019-10-31 18:17:42 +0300

    Project preparation #1

---
+++ ecloud/client/src/main/java/com/ec/client/MainClient.java
@@ -0,0 +1,25 @@
+package com.ec.client;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainClient extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/main.xml"));
+        Parent root = fxmlLoader.load();
+        primaryStage.setTitle("Box");
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
