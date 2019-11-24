9cec127103e53763455ae820f0f50fda5810245c
8Seph <evem-w@outlook.com>
2019-11-24 22:11:38 +0300

    Update ClientCommandHandler.java

---
+++ ecloud/client/src/main/java/com/ec/client/ClientCommandHandler.java
@@ -0,0 +1,49 @@
+package com.ec.client;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.io.IOException;
+
+public class ClientCommandHandler extends ChannelInboundHandlerAdapter {
+
+    public ClientCommandHandler(MainController controller) {
+        this.controller = controller;
+    }
+    private MainController controller;
+    protected static boolean downFlag = false;
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws IOException {
+        ByteBuf byteBuf = (ByteBuf) msg;
+
+        if (!downFlag){
+            int command = byteBuf.readByte();
+            System.out.println("HANDLER:\nCommand: " + command);
+
+            // Начало загрузки файла с сервера
+            if (command == 66){
+                downFlag = true;
+                ClientCommandManager.downloadFile(ctx, byteBuf);
+            }
+
+            // Получение списка файлов
+            if (command == 25) {
+                ClientCommandManager.updateServerFileList(ctx, byteBuf, controller);
+            }
+
+        } else {
+            /// продолжение загрузки файла
+            ClientCommandManager.downloadFile(ctx, byteBuf);
+        }
+
+    }
+
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
